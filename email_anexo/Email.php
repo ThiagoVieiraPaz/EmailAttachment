d1617fdd3d6b9a82b4016734fec7f0b6dfd774b9
Thiago da Paz <thiagopaz@thiagopaz.com.br>
2015-07-25 01:16:32 -0300

    Enviando Emails no PHP com Anexos!
    Uma maneira simples de enviar emails com anexos no PHP.

---
+++ email_anexo/Email.php
@@ -0,0 +1,184 @@
+﻿<?php
+class Email{
+	var $sender;
+	var $destiny;
+	var $file;
+	var $copy;
+	var $subject;
+	var $message;
+	var $rn;
+	public function __construct(){
+		if( PATH_SEPARATOR ==';'){
+			$this->rn = "\r\n"; 
+		} elseif (PATH_SEPARATOR==':'){
+			$this->rn = "\n";	 
+		} elseif ( PATH_SEPARATOR!=';' and PATH_SEPARATOR!=':' )  {
+			echo ('Esse Script Não Funcionará Corretamente!');		 
+		}
+	}
+	/*
+		Setar Arquivo
+	*/
+	public function setAttachment($arquivo){
+		$arquivo  = $this->reArrayFiles($arquivo);
+		$this->file = $arquivo;			
+	}
+	/*
+		Recuperar Arquivo
+	*/
+	public function getAttachment(){
+		return $this->file;	
+	}
+	/*
+		Setar Remetente	
+	*/
+	public function setSender($email){
+		$this->sender = $email;	
+	}
+	/*
+		Recuperar Remetente	
+	*/
+	public function getSender(){
+		return $this->sender;	
+	}
+	/*
+		Setar Destinatário
+	*/
+	public function setDestiny($email){
+		$this->destiny = $email;	
+	}
+	/*
+		Recuperar Destinatário
+	*/
+	public function getDestiny(){
+		return $this->destiny;	
+	}
+	/*
+		Setar Cópia Oculta
+	*/
+	public function setCopy($email){
+		$this->copy = $email;	
+	}
+	/*
+		Recuperar Cópia Oculta
+	*/
+	public function getCopy(){
+		return $this->copy;	
+	}
+	/*
+		Recuperar Assunto
+	*/
+	public function getSubject(){
+		return $this->subject;	
+	}
+	/*
+		Setar Assunto
+	*/
+	public function setSubject($assunto){
+		$this->subject= $assunto;	
+	}	
+	/*
+		Recuperar Mensagem
+	*/
+	public function getBody(){
+		return $this->message;	
+	}
+	/*
+		Setar Assunto
+	*/
+	public function setBody($message){
+		$this->message = $message;	
+	}
+	/*
+		Enviar!
+	*/
+	public function send(){
+		$arquivos = $this->getAttachment();
+		$rn = $this->rn;
+		$assunto = $this->getSubject();
+		$email = $this->getDestiny();
+		$email_from = $this->getSender();
+		$mensagem = $this->getBody();
+
+		if($this->checkFiles($arquivos)){				 
+			$boundary = "XYZ-" . date("dmYis") . "-ZYX"; 
+			$message = "--$boundary" . $rn; 
+			$message .= "Content-Transfer-Encoding: 8bits" . $rn; 
+			$message .= "Content-Type: text/html; charset=\"UTF-8\"" . $rn . "" . $rn;
+			$message .= "$mensagem" . $rn . $rn; 			
+			 
+			foreach($arquivos as $arquivo):	
+				//print_r($arquivo);	
+				if($arquivo["size"] > 0){
+					$fp = fopen($arquivo["tmp_name"],"rb"); 
+					$anexo = fread($fp,filesize($arquivo["tmp_name"])); 
+					$anexo = base64_encode($anexo); 
+					$type = $arquivo["type"];
+					fclose($fp);		
+					 
+					$anexo = chunk_split($anexo); 			
+					$message .= "--$boundary" . $rn;	
+					$message .= "Content-Type: ".$type.$rn; 	
+					$message .= "Content-Disposition: attachment; filename=\"".$arquivo["name"]."\"" . $rn; 
+					$message .= "Content-Transfer-Encoding: base64" . $rn . "" . $rn;
+					$message .= "$anexo" . $rn; 
+					$mens .= "--$boundary--" . $rn; 
+ 
+					$headers = "MIME-Version: 1.0" . $rn; 
+					$headers .= "From: $email_from " . $rn ; 
+					$headers .= "Return-Path: $email_from " . $rn; 
+					$headers .= "Content-type: multipart/mixed; boundary=\"$boundary\"" . $rn; 
+					$headers .= "$boundary" . $rn; 
+				}
+			endforeach;
+		}
+		else{
+			$message = "$mensagem" . $rn . $rn; 		
+		}
+		
+		$enviado = mail($email,$assunto,$message,$headers, "-r".$email_from); 		
+
+		if($enviado){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	/*
+		Reordena Array!
+	*/
+	public function reArrayFiles(&$file_post) {
+		$file_array = array();
+		$file_count = count($file_post['name']);
+		$file_keys = array_keys($file_post);
+	
+		for ($i=0; $i<$file_count; $i++) {
+			foreach ($file_keys as $key) {
+				$file_array[$i][$key] = $file_post[$key][$i];
+			}
+		}
+	
+		return $file_array;
+	}
+	/*
+		Verifica cada arquivo se possui tamanho maior do que Zero (0)
+	*/
+	public function checkFiles($arquivos){
+		$i = 0;
+		foreach($arquivos as $arquivo):			
+			if($arquivo["size"] > 0){
+				$i++;
+			}
+		endforeach;
+		return $i;
+	}
+	
+	
+	public function __destruct(){
+		
+	}
+
+}
+
+?>
---
+++ email_anexo/index.php
@@ -0,0 +1,91 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Enviar Email com Anexo</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<?
+	include("Email.php");
+	//Caso os arquivos sejam vários arquivos
+	set_time_limit(0);	
+	if(isset($_POST["send"])){
+		extract($_POST);
+		$email = new Email();
+			$email->setAttachment($_FILES["arquivo"]);
+			$email->setSender($remetente);
+			$email->setDestiny($destinatario);
+			$email->setCopy($copia);
+			$email->setSubject($assunto);
+			$email->setBody($mensagem);
+		$check = $email->send();
+		if($check){
+			echo "<script>alert('Email enviado com Sucesso!');</script>";
+		}
+		else{
+			echo "<script>alert('Houve um erro ao processar seu pedido!');</script>";
+		}
+	}
+?>
+<h3>Thiago Paz  - www.programador.me </h3>
+<a href="https://github.com/ThiagoVieiraPaz/" target="new"><img src="http://programador.me/public/images/bt_git.png"></a>
+<h1>Enviar email com Anexo!</h1>
+<hr>
+<div class="col-md-5">
+<form action="" method="post" enctype="multipart/form-data">
+ <div class="form-group">
+    <label for="remetente">Remetente</label>
+    <input name="remetente" type="email" required="required" class="form-control" id="remetente" placeholder="Email Remetente" value="thiagopaz@thiagopaz.com.br">
+ </div>
+
+<div class="form-group">
+    <label for="destinatario">Destinatário</label>
+    <input name="destinatario" type="email" required="required" class="form-control" id="destinatario" placeholder="Email Destinatário" value="programadorpaz@gmail.com">
+ </div>
+ 
+<div class="form-group">
+    <label for="copia">Cópia Oculta</label>
+    <input name="copia" type="email" class="form-control" id="copia" placeholder="Cópia Oculta" value="contato@programador.me">
+ </div>
+ 
+ <div class="form-group">
+    <label for="assunto">Assunto</label>
+    <input name="assunto" type="text" class="form-control" id="assunto" placeholder="Assunto" value="Teste de Assunto">
+ </div>
+ 
+ <div class="form-group">
+    <label for="Mensagem">Mensagem</label>
+    <textarea name="mensagem" class="form-control" id="mensagem" placeholder="Mensagem">Olá Teste de Mensagem</textarea>
+ </div>
+ <div class="form-group">
+    <label for="arquivo">Arquivo</label>
+    <input name="arquivo[]" type="file" id="arquivo">
+    <p class="help-block">Arquivos de qualquer Tipo.</p>
+  </div>
+  <div class="form-group">
+    <label for="arquivo">Arquivo</label>
+    <input type="file" id="arquivo" name="arquivo[]">
+    <p class="help-block">Arquivos de qualquer Tipo.</p>
+  </div>
+  <!-- Quantos Quiser :) 
+   <div class="form-group">
+    <label for="arquivo">Arquivo</label>
+    <input type="file" id="arquivo" name="arquivo[]">
+    <p class="help-block">Arquivos de qualquer Tipo.</p>
+  </div>
+  -->
+ <button type="submit" class="btn btn-default" name="send" value="enviar">Enviar</button>
+</form>
+</div>
+</body>
+</html>
